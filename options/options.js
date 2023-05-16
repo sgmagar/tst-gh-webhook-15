1005e5f15da58223bcca2d34c03feabb3eafe500
Saurav <sp.gharti@gmail.com>
2023-05-16 07:56:49 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "tst-gh-webhook-15",
+  url: "https://tst-gh-webhook-15.botics.co",
+  api: "https://tst-gh-webhook-15.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
