ac73dc15fdb0c0c439a2cfe9799aa3c5753cb246
Saurav Gharti <sp.gharti@gmail.com>
2023-05-22 19:15:25 +0545

    Update models.py

--- backend/home/models.py
+++ backend/home/models.py
@@ -4,4 +4,4 @@ class Hello(models.Model):
     hey = models.CharField(max_length=100)
     
     def __str__(self):
-        return f"Something {self.hey}"
+        return f"Something {self.hey} hey"
