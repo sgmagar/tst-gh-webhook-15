f605ae5ee5111df5dd1a55493d1da4f8acfbcab6
Saurav Gharti <sp.gharti@gmail.com>
2023-05-23 14:38:58 +0545

    Update models.py

--- backend/home/models.py
+++ backend/home/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 class Hello(models.Model):
     hey = models.CharField(max_length=100)
+    hi = models.CharField(max_length=200)
     
     def __str__(self):
         return f"Something {self.hey} heyyyyy"
